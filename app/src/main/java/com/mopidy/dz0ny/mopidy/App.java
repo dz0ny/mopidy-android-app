2da7aa8cfbca58ec2910aa088f4fa7c6ed60dc2c
dz0ny <dz0ny@ubuntu.si>
2014-07-31 13:01:42 +0200

    Add analyitcs

--- app/src/main/java/com/mopidy/dz0ny/mopidy/App.java
+++ app/src/main/java/com/mopidy/dz0ny/mopidy/App.java
@@ -2,6 +2,9 @@ package com.mopidy.dz0ny.mopidy;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
 import com.joshdholtz.sentry.Sentry;
 import com.mopidy.dz0ny.mopidy.api.AutoUpdate;
 import com.mopidy.dz0ny.mopidy.services.Discovery;
@@ -11,6 +14,9 @@ import timber.log.Timber;
 import static timber.log.Timber.DebugTree;
 
 public class App extends Application {
+
+    private Tracker tracker;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -21,8 +27,20 @@ public class App extends Application {
         if (BuildConfig.DEBUG) {
             Timber.plant(new DebugTree());
         }
+        getTracker().enableAutoActivityTracking(true);
+        getTracker().send(new HitBuilders.EventBuilder()
+                .setCategory("app")
+                .setAction("startup")
+                .setLabel("new")
+                .build());
     }
-
+    synchronized Tracker getTracker() {
+        if (tracker == null) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            tracker = analytics.newTracker(R.xml.tracker);
+        }
+        return tracker;
+    }
     @Override
     public void onTerminate() {
         Discovery.Stop(this);
